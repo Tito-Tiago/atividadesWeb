18c5cc9145a656baec72e7d94fb37440919df238
Tiago Tito <tiago.tito.sampaio@gmail.com>
2025-05-20 20:50:01 -0300

    pronto

--- atv4/script.js
+++ atv4/script.js
@@ -1,4 +1,5 @@
 function toggleModes(){
+    // Referência à tag <link> do tema pelo id
     let tema = document.getElementById("tema")
     if(tema.href.endsWith("styleLight.css")){
         tema.href = "styleDark.css"
